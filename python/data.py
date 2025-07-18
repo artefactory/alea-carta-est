8bf277bba7890b81e6061016154b36cb5f48ec54
Vincent Auriau <auriau.vincent@gmail.com>
2025-07-18 15:59:50 +0200

    ADD:  example notebook, small CI, & more (#5)

--- python/data.py
+++ python/data.py
@@ -1,45 +1,40 @@
 """Data generation related stuff."""
 
 import numpy as np
-from tqdm import trange
 
 
 class SyntheticDataGenerator:
+    """Generate Synthetic Data."""
+
     def __init__(
         self,
-        items_nest: dict, # keys should be integer: the nest number
+        items_nest: dict,  # keys should be integer: the nest number
         nests_interactions: list,
         proba_complementary_items: float = 0.7,
         proba_neutral_items: float = 0.15,
         noise_proba: float = 0.05,
     ) -> None:
-
         self.proba_complementary_items = proba_complementary_items
         self.proba_neutral_items = proba_neutral_items
         self.noise_proba = noise_proba
-        
-  
+
         self.items_nest = items_nest
         self.nests_interactions = nests_interactions
 
     def generate_basket(self) -> list:
-        """Generates a basket of items based on the defined item sets and their relations."""
-
+        """Generate a basket of items based on the defined item sets and their relations."""
 
         def select_first_item() -> tuple:
-            """Selects the first item and its nest randomly from the available sets."""
-
+            """Select the first item and its nest randomly from the available sets."""
             chosen_nest = np.random.choice(list(self.items_nest.keys()))
             chosen_item = np.random.choice(list(self.items_nest[chosen_nest]))
             return chosen_item, chosen_nest
 
         def complete_basket(first_item: int, first_nest: str) -> list:
-            """Completes the basket by adding items based on the relations of the first item."""
-
+            """Complete the basket by adding items based on the relations of the first item."""
             basket = [first_item]
-            first_key_index = first_nest
             relations = self.nests_interactions[first_nest]
-            for nest_id, items in self.items_nest:
+            for nest_id, items in self.items_nest.items():
                 if (
                     relations[nest_id] == "compl"
                     and np.random.random() < self.proba_complementary_items
@@ -53,7 +48,7 @@ class SyntheticDataGenerator:
             return basket
 
         def add_noise(basket: list) -> list:
-            """Adds noise items to the basket based on the defined noise probability."""
+            """Add noise items to the basket based on the defined noise probability."""
             if np.random.random() < self.noise_proba:
                 possible_noisy_items = []
                 for nest, items in self.items_nest.items():
@@ -65,17 +60,12 @@ class SyntheticDataGenerator:
             return basket
 
         first_chosen_item, first_chosen_nest = select_first_item()
-        basket = complete_basket(first_chosen_item, first_chosen_nest)
-        basket = add_noise(basket)
-
-        return basket
+        basket = complete_basket(first_item=first_chosen_item, first_nest=first_chosen_nest)
+        return add_noise(basket)
 
     def generate_dataset(self, n_baskets) -> np.ndarray:
-        """Generates a dataset of baskets."""
-
+        """Generate a dataset of baskets."""
         baskets = []
         for _ in range(n_baskets):
             baskets.append(self.generate_basket())
         return np.array(baskets, dtype=object)
-    
-    
